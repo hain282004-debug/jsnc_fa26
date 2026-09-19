27289ce2ec88f85c5d87dbc4b3d901431b2dba23
hai <hain282004@gmail.com>
2026-09-19 15:13:39 +0700

    lab2

---
+++ lab2.js
@@ -0,0 +1,42 @@
+// const student1 = "Nguyễn Văn An";
+// const student2 = "Trần Văn Bình";
+// const student3 = "Lê Văn Nam";
+
+// const students = ["Nguyễn Văn An", "Trần Văn Bình", "Lê Văn Nam"];
+// const ids = [1,5,10,45,54];
+// const actives = [true, false, 42, "hainv"];
+
+// students[0]="nox";
+// console.log(students[0]);
+
+// console.log(students[10]);
+
+// console.log("sau khi thay doi",students);
+
+// //for
+
+// for (let i = 0; i < students.length; i++) {
+//   console.log(students[i]);
+// }
+
+// Bài 1 
+const names = ["An", "Bình", "Nam", "Hòa", "Lan"];
+console.log(names);
+console.log(names[0]);
+console.log(names[4]);
+console.log(names.length);
+
+names.push("Nox");
+console.log(names);
+
+names.pop();
+console.log(names);
+
+
+//Bài 2
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i]);
+}
+
+
+//Bài3
