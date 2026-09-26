33fbed24673898cdd2548c753fdc2b9c4fb191be
hai <hain282004@gmail.com>
2026-09-26 15:38:36 +0700

    btaplab4

---
+++ products.js
@@ -0,0 +1,31 @@
+axios.get("http://localhost:3000/products").then((res)=>{
+    console.log("Products",res.data);
+    document.getElementById("products").innerHTML=res.data.map(
+        (item,index)=>`
+        <tr class="hover:bg-gray-50">
+        <td class="px-4 py-2 border border-gray-300">${index + 1}</td>
+              <td class="px-4 py-2 border border-gray-300">${item.id}</td>
+              <td class="px-4 py-2 border border-gray-300">${item.name}</td>
+              <td class="px-4 py-2 border border-gray-300">${item.price}</td>
+                            <td class="px-4 py-2 border border-gray-300">${item.category}</td>
+
+              <td class="px-4 py-2 border border-gray-300">
+                <div class="flex items-center justify-center gap-2">
+                  <a
+                    href="#"
+                    class="bg-blue-500 hover:bg-blue-600 text-white px-3 py-1 rounded"
+                  >
+                    Edit
+                  </a>
+
+                  <button
+                    class="bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </td>
+            </tr>
+    `,
+    ).join("");
+});
